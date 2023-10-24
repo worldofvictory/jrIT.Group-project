65f829f042644866cef4399d721252a53070cf6e
Anna <anjanadvorna@gmail.com>
2023-10-24 15:20:01 +0300

    fix

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { deleteCard } from "./local-storage";
 
 async function renderQuote() {
     const currentDate = new Date();
@@ -28,7 +29,7 @@ async function serviceQuote() {
     const response = await axios.get(
       `https://your-energy.b.goit.study/api/quote`
     );
-    return response.data; // Повертаємо дані з API
+    return response.data; 
   } catch (error) {
     console.error('Помилка отримання даних: ', error);
     return null;
@@ -37,7 +38,7 @@ async function serviceQuote() {
 
 renderQuote();
 
-const exData = [
+let exData = [
   {
     "_id": "64f389465ae26083f39b17a7",
     "bodyPart": "chest",
@@ -64,177 +65,229 @@ const exData = [
     "time": 3,
     "popularity": 5
   },
-  {
-    "_id": "64f389465ae26083f39b1833",
-    "bodyPart": "chest",
-    "equipment": "cable",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0169.gif",
-    "name": "cable incline bench press",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 58,
-    "time": 3,
-    "popularity": 0
-  },
-  {
-    "_id": "64f389465ae26083f39b183d",
-    "bodyPart": "chest",
-    "equipment": "cable",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0179.gif",
-    "name": "cable low fly",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 76,
-    "time": 3,
-    "popularity": 0
-  },
-  {
-    "_id": "64f389465ae26083f39b189e",
-    "bodyPart": "chest",
-    "equipment": "dumbbell",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0302.gif",
-    "name": "dumbbell decline fly",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 4.33,
-    "burnedCalories": 67,
-    "time": 3,
-    "popularity": 600
-  },
-  {
-    "_id": "64f389465ae26083f39b189f",
-    "bodyPart": "chest",
-    "equipment": "dumbbell",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0303.gif",
-    "name": "dumbbell decline hammer press",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 253,
-    "time": 3,
-    "popularity": 1
-  },
-  {
-    "_id": "64f389465ae26083f39b1946",
-    "bodyPart": "chest",
-    "equipment": "body weight",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0494.gif",
-    "name": "incline reverse grip push-up",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 216,
-    "time": 3,
-    "popularity": 0
-  },
-  {
-    "_id": "64f389465ae26083f39b1960",
-    "bodyPart": "chest",
-    "equipment": "kettlebell",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0531.gif",
-    "name": "kettlebell extended range one arm press on floor",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 284,
-    "time": 3,
-    "popularity": 0
-  },
-  {
-    "_id": "64f389465ae26083f39b17be",
-    "bodyPart": "chest",
-    "equipment": "barbell",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0033.gif",
-    "name": "barbell decline bench press",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 129,
-    "time": 3,
-    "popularity": 0
-  },
-  {
-    "_id": "64f389465ae26083f39b17cc",
-    "bodyPart": "chest",
-    "equipment": "barbell",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0047.gif",
-    "name": "barbell incline bench press",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 165,
-    "time": 3,
-    "popularity": 0
-  },
-  {
-    "_id": "64f389465ae26083f39b1841",
-    "bodyPart": "chest",
-    "equipment": "cable",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0185.gif",
-    "name": "cable lying fly",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 61,
-    "time": 3,
-    "popularity": 1
-  },
-  {
-    "_id": "64f389465ae26083f39b187f",
-    "bodyPart": "chest",
-    "equipment": "body weight",
-    "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0258.gif",
-    "name": "clock push-up",
-    "target": "pectorals",
-    "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
-    "rating": 3,
-    "burnedCalories": 48,
-    "time": 3,
-    "popularity": 0
-  }
+  // {
+  //   "_id": "64f389465ae26083f39b1833",
+  //   "bodyPart": "chest",
+  //   "equipment": "cable",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0169.gif",
+  //   "name": "cable incline bench press",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 58,
+  //   "time": 3,
+  //   "popularity": 0
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b183d",
+  //   "bodyPart": "chest",
+  //   "equipment": "cable",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0179.gif",
+  //   "name": "cable low fly",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 76,
+  //   "time": 3,
+  //   "popularity": 0
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b189e",
+  //   "bodyPart": "chest",
+  //   "equipment": "dumbbell",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0302.gif",
+  //   "name": "dumbbell decline fly",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 4.33,
+  //   "burnedCalories": 67,
+  //   "time": 3,
+  //   "popularity": 600
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b189f",
+  //   "bodyPart": "chest",
+  //   "equipment": "dumbbell",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0303.gif",
+  //   "name": "dumbbell decline hammer press",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 253,
+  //   "time": 3,
+  //   "popularity": 1
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b1946",
+  //   "bodyPart": "chest",
+  //   "equipment": "body weight",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0494.gif",
+  //   "name": "incline reverse grip push-up",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 216,
+  //   "time": 3,
+  //   "popularity": 0
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b1960",
+  //   "bodyPart": "chest",
+  //   "equipment": "kettlebell",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0531.gif",
+  //   "name": "kettlebell extended range one arm press on floor",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 284,
+  //   "time": 3,
+  //   "popularity": 0
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b17be",
+  //   "bodyPart": "chest",
+  //   "equipment": "barbell",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0033.gif",
+  //   "name": "barbell decline bench press",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 129,
+  //   "time": 3,
+  //   "popularity": 0
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b17cc",
+  //   "bodyPart": "chest",
+  //   "equipment": "barbell",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0047.gif",
+  //   "name": "barbell incline bench press",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 165,
+  //   "time": 3,
+  //   "popularity": 0
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b1841",
+  //   "bodyPart": "chest",
+  //   "equipment": "cable",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0185.gif",
+  //   "name": "cable lying fly",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 61,
+  //   "time": 3,
+  //   "popularity": 1
+  // },
+  // {
+  //   "_id": "64f389465ae26083f39b187f",
+  //   "bodyPart": "chest",
+  //   "equipment": "body weight",
+  //   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0258.gif",
+  //   "name": "clock push-up",
+  //   "target": "pectorals",
+  //   "description": "These are the large chest muscles responsible for shoulder adduction and horizontal adduction. Bench press, push-ups, and chest flies target these muscles.",
+  //   "rating": 3,
+  //   "burnedCalories": 48,
+  //   "time": 3,
+  //   "popularity": 0
+  // }
 ]
 
-const galleryList = document.querySelector(".gallery-list");
-const galleryContainer = document.querySelector(".favorites-gallery"); 
-const defaultText = document.querySelector(".js-hidden-text")
+const Refs = {
+  galleryList: document.querySelector(".gallery-list"),
+  galleryContainer: document.querySelector(".favorites-gallery"),
+  defaultText: document.querySelector(".js-hidden-text"),
+  removeButton: document.querySelector(".trash-btn"),
+}
+
 function createExerciseCards(exercises) {
+  
     return exercises.map(({_id, bodyPart, burnedCalories, target, name }) => 
-      `<li data-id="${_id}" class="test-card">
-      <div class="exersise-header">
-      <div>
-      <p class="workout">WORKOUT</p>
-        <svg width="16" height="16">
-                  <use href="./img/sprite.svg#icon-dumbbell-black"></use>
-                  </svg>
-     
-      </div>
-       
-       
-                  <button class="exercise-btn" type="button">Start<svg width="16" height="16">
-                  <use href="./img/sprite.svg#icon-dumbbell-black"></use>
-                  </svg>
-                  </button>
-           </div>        
-            <p class="exercise-name">${capitalize(name)}</p>
-            <p>Burned calories:<spsn>${burnedCalories}</spsn></p>
-            <p>Body part:<spsn>${bodyPart}</spsn></p>
-            <p>Target:<spsn>${target}</spsn></p>
-        </li>`
+      `<li data-id="${_id}" class="exercise-card">
+        <div class="exersise-header">
+          <div class="exercise-trash">
+            <p class="workout">WORKOUT</p>
+            <button class="trash-btn" type="submit"></bytton>
+              <svg class="trash-svg"width="16" height="16">
+                <use href="./img/sprite.svg#icon-trash-modal"></use>
+                </svg>     
+          </div>    
+            <button class="exercise-btn" type="button">Start
+              <svg class="arrow-svg"width="16" height="16">
+                <use href="./img/sprite.svg#icon-arrow"></use>
+              </svg>
+            </button>
+        </div>  
+          <div class = "exercise-tittle"> 
+            <svg class="man-svg-quote" width="24" height="24">
+              <use href="./img/sprite.svg#icon-icon-2"></use>
+            </svg>
+            <p class="favorite-exercise-name">${capitalize(name)}</p>
+          </div> 
+          <div class="exercice-information">
+            <p class="exercise-category">Burned calories: <span>${burnedCalories}</span></p>
+            <p class="exercise-category">Body part: <span>${bodyPart}</span></p>
+            <p class="exercise-category">Target: <span>${target}</span></p>
+          </div>            
+      </li>`
     ).join("");
 }
 
 function renderCards() {
-  let exercises = exData
+  
+  let exercises = exData;
 
   let cards = createExerciseCards(exercises);
-  galleryList.insertAdjacentHTML('beforeend', cards);
-  hide(defaultText);
-  show(galleryContainer);
+  Refs.galleryList.insertAdjacentHTML('beforeend', cards);
+
+  hide(Refs.defaultText);
+  show(Refs.galleryContainer);
 }
 
-renderCards()
+renderCards(); 
+
+const removeButtons = document.querySelectorAll(".trash-btn");
+const cards = document.querySelectorAll(".exercise-card"); 
+const card = document.querySelector(".exercise-card"); 
+
+// for (let button of removeButtons) {
+//   button.addEventListener('click', removeCard);
+ 
+// }
+
+// function removeCard(event) {
+//   console.log("clisk");
+//   console.dir(event.currentTarget)
+
+// }
+console.log(cards)
+
+
+console.log(cards)
+for (let card of cards) {
+  card.addEventListener('click', removeCard);
+ 
+}
+
+function removeCard(event) {
+  
+   const id = event.currentTarget.dataset.id; 
+  
+  deleteCard(id);
+
+  
+  // Refs.galleryList.remove()
+
+  // renderCards();
+  
+  card.remove();
+  console.log("+");
+  // renderCards();
+}
 
 
 function hide(element) {
@@ -246,5 +299,5 @@ function show(element) {
 }
 
 function capitalize(s) {
-  return s[0].toUpperCase() + s.slice(1).toLowerCase();
+  return s[0].toUpperCase() + s.slice(1);
 }
