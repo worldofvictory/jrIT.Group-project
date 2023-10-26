198d99e0506115367ee5acd11ece3e8dfc136639
Yara1504 <baykenichs@gmail.com>
2023-10-26 09:28:14 +0300

    filter change

--- src/js/filter-2.js
+++ src/js/filter-2.js
@@ -1,111 +1,159 @@
-import Pagination from "tui-pagination";
-import 'tui-pagination/dist/tui-pagination.css';
-
+import Pagination from "tui-pagination"; 
+import 'tui-pagination/dist/tui-pagination.css'; 
+import axios from "axios";
+import { handleOpenModalClick } from './modal-exercise'; 
+ 
 const form = document.querySelector(".search-form");
 const searchInput = document.querySelector(".search-input");
 const exercisesBack = document.querySelector(".exercises-back");
 const iconSearch = document.getElementById('icon-search');
 const iconX = document.getElementById('icon-x');
+const filterBtn = document.querySelectorAll(".filter-btn");
 
 const apiUrl = "https://your-energy.b.goit.study/api";
 
-fetchExercises("");
+let activeFilter = 'Body parts';
+let activeCategory = 'Body parts';
+let activePage = 1;
+const itemsPerPage = 12;
 
-form.addEventListener('submit', searchExercises);
-searchInput.addEventListener('input', startSearch);
+const filterParams = {
+    filter: activeFilter,
+    category: activeCategory,
+    keyword: '',
+};
 
-function fetchExercises(keyword) {
-    const categories = ["back", "cardio", "chest", "lower arms", "lower legs", "neck", "shoulders", "upper arms", "upper legs", "waist"];
-    
-    
-const promises = categories.map(category => {
-        return fetch(`${apiUrl}/exercises?keyword=${keyword}&bodypart=${category}&limit=12`)
-            .then(response => {
-                if (!response.ok) {
-                     throw new Error(`Sorry, is not found`);
-                }
-                return response.json();
-            });
-    })
-Promise.all(promises)
-    .then(data => {
-        exercisesBack.innerHTML = "";
-        data.forEach(categoryData => {
-            handleExerciseData(categoryData.results);
+export function fetchExercises() {
+    const { filter, category, keyword } = filterParams;
+    fetch(`${apiUrl}/exercises?filter=${filter}&category=${category}&keyword=${keyword}&page=${activePage}&limit=${itemsPerPage}`)
+        .then(response => response.json())
+        .then(data => {
+            handleExerciseData();
+        })
+        .catch(error => {
+            console.error('Sorry, is not found', error);
+            return {
+                success: false
+            }
         });
-    })
-    .catch(error => {
-        console.error(error);
-    });
 }
 
-function searchExercises(event) {
+filterBtn.forEach(element => {
+    element.addEventListener('click', event => {
+        filterBtn.forEach(btn => btn.classList.remove('current'));
+        element.classList.add('current');
+
+        filterParams.filter = event.target.dataset.filter;
+        filterParams.category = event.target.dataset.filter;
+        activePage = 1;
+
+        fetchExercises();
+    });
+});
+
+form.addEventListener('submit', event => {
     event.preventDefault();
     const searchValue = searchInput.value.toLowerCase();
+    filterParams.keyword = searchValue;
+    activePage = 1;
+    fetchExercises();
+});
 
-    fetchExercises(searchValue);
-}
+function handleExerciseData(results) { 
+    exercisesBack.innerHTML = ""; 
+    if (results.length === 0) { 
+        exercisesBack.innerHTML = "Sorry, is not found"; 
+    } else { 
+        results.forEach((exercise) => { 
+            const infoCard = createInfoCard(exercise); 
+            infoCard.addEventListener('click', onStartClick); 
+            exercisesBack.appendChild(infoCard);         
+        }); 
+    } 
+} 
+ 
+async function onStartClick(event) { 
+    console.log(event.target); 
+        if (event.target.id !== "ok" && event.target.id !== "icon-arrow") { 
+        console.log("+") 
+        return;  
+    } 
+        
+    const id = event.currentTarget.dataset.id;  
+       handleOpenModalClick(event, id); 
+} 
 
-function handleExerciseData(results) {
-    exercisesBack.innerHTML = "";
-    if (results.length === 0) {
-        exercisesBack.innerHTML = "Sorry, is not found";
-    } else {
-        results.forEach((exercise) => {
-            const infoCard = createInfoCard(exercise);
-            exercisesBack.appendChild(infoCard);
-        });
-    }
-}
-function startSearch() {
-    const searchValue = searchInput.value.toLowerCase();
-    fetchExercises(searchValue);
-}
+function createInfoCard(exercise) { 
+    const exerciseCard = document.createElement("li"); 
+    exerciseCard.classList.add("exCard"); 
+    exerciseCard.dataset.id = exercise._id;  
+   
+    exerciseCard.innerHTML = ` 
+    <div class="high-card"> 
+    <div class="left-high-card"> 
+    <div class="workout-symbol"> 
+        <p class="workout-p">WORKOUT</p> 
+        </div> 
+    <div class="ex-rating"> 
+        <p class="rating">${exercise.rating}</p> 
+        <svg id="icon-star" width="18" height="18"> 
+        <use href="./img/sprite.svg#icon-star-yellow"></use> 
+        </svg> 
+         </div> 
+    </div> 
+        <button id ="ok" class="open-modal-exercises" > 
+        Start 
+        <svg id="icon-arrow" width="16" height="16"> 
+            <use href="./img/sprite.svg#icon-arrow"></use> 
+        </svg> 
+    </button> 
+    </div> 
+ 
+    <div class="exercise-name"> 
+        <svg id="icon-run" width="24" height="24"> 
+        <use href="./img/sprite.svg#icon-icon-run"></use> 
+        </svg> 
+        <h3 class="ex-name">${exercise.name}</h3> 
+    </div> 
+    <div class="exercise-info"> 
+        <p class="ex-info-p">Burned calories: <span class="ex-info-back">${exercise.burnedCalories} / ${exercise.time}</span></p> 
+        <p class="ex-info-p">Body part: <span class="ex-info-back">${exercise.bodyPart}</span></p> 
+        <p class="ex-info-p last-p">Target: <span class="ex-info-back">${exercise.target}</span></p> 
+    </div> 
+    
+    `; 
+  
+ 
+    return exerciseCard; 
+} 
 
-function createInfoCard(exercise) {
-    const exerciseCard = document.createElement("li");
-    exerciseCard.classList.add("exCard");
 
-    exerciseCard.innerHTML = `
-    <div class="high-card">
-    <div class="left-high-card">
-    <div class="workout-symbol">
-        <p class="workout-p">WORKOUT</p>
-        </div>
-    <div class="ex-rating">
-        <p class="rating">${exercise.rating}</p>
-        <svg id="icon-star" width="18" height="18">
-        <use href="./img/sprite.svg#icon-star-yellow"></use>
-        </svg>
-         </div>
-    </div>
-        <div class="open-modal-exercises" >
-        <button class="filter-btn-ex" type="button" data-modal-exercise="open"><p class="start-btn">Start<svg id="icon-arrow" width="16" height="16">
-            <use href="./img/sprite.svg#icon-arrow"></use>
-        </svg></p></button>
-    </div>
-    </div>
-
-    <div class="exercise-name">
-        <svg id="icon-run" width="24" height="24">
-        <use href="./img/sprite.svg#icon-icon-run"></use>
-        </svg>
-        <h3 class="ex-name">${exercise.name}</h3>
-    </div>
-    <div class="exercise-info">
-        <p class="ex-info-p">Burned calories: <span class="ex-info-back">${exercise.burnedCalories} / ${exercise.time}</span></p>
-        <p class="ex-info-p">Body part: <span class="ex-info-back">${exercise.bodyPart}</span></p>
-        <p class="ex-info-p last-p">Target: <span class="ex-info-back">${exercise.target}</span></p>
-    </div>
-    `;
-    return exerciseCard;
-}
-searchInput.addEventListener('focus', function() {
-        iconSearch.style.display = 'none';
-        iconX.style.display = 'block';
+ function getExercises({ filter, name }) {
+  const filterParamMap = {
+    'Body parts': 'bodypart',
+    'muscles': 'muscles',
+    'equipment': 'equipment'
+  };
+  const filterParam = filterParamMap[filter];
+  
+  fetch(`${apIUrl}/exercises?${filterParam}=${name}&page=${activePage}&limit=${activeLimit}`)
+    .then(response => response.json())
+    .then(data => {
+      handleExerciseResponse(data);
+    })
+    .catch(error => {
+      console.error('Error while fetching exercises:', error);
     });
-    searchInput.addEventListener('blur', function() {
-        iconSearch.style.display = 'block';
-        iconX.style.display = 'none';
-        searchInput.value = '';
+}
+
+searchInput.addEventListener('focus', function() { 
+        iconSearch.style.display = 'none'; 
+        iconX.style.display = 'block'; 
     });
+searchInput.addEventListener('blur', function() { 
+        iconSearch.style.display = 'block'; 
+        iconX.style.display = 'none'; 
+        searchInput.value = ''; 
+    });
+
+
