d69e4a83c36aee0f3bd749bae1ba607d88159555
Anna <anjanadvorna@gmail.com>
2023-10-27 01:30:49 +0300

    dots

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -218,14 +218,14 @@ function createExerciseCards(exercises) {
           </div>    
             <button id = "open" class="exercise-btn" type="button">Start
               <svg id = "arrow" class="arrow-svg" width="16" height="16">
-                <use href="./img/sprite.svg#icon-arrow"></use>
+                <use href="../img/sprite.svg#icon-arrow"></use>
               </svg>
             </button>
         </div>  
           <div class = "exercise-tittle"> 
           <div class= "man-svg-thumb">
             <svg width="24" height="24">
-              <use href="./img/sprite.svg#icon-icon-2"></use>
+              <use href="../img/sprite.svg#icon-icon-2"></use>
             </svg>
             </div>
             <p class="favorite-exercise-name">${capitalize(name)}</p>
